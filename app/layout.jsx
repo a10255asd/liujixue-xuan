8fa6e3833ca8be49619fa4fa5365bfc3673924d3
刘鸡血 <1025519998@qq.com>
2026-07-16 06:39:26 +0800

    Add xuan viewport theme metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -38,7 +38,9 @@ export const metadata = {
 
 export const viewport = {
   width: 'device-width',
-  initialScale: 1
+  initialScale: 1,
+  themeColor: '#f4efe6',
+  colorScheme: 'light'
 }
 
 export default function RootLayout({ children }) {
